d4abdb98d83a57d5432011a6f502461d26dc7ac6
akatsenis <49352680+akatsenis@users.noreply.github.com>
2026-04-18 14:20:02 +0300

    Delete pages directory

--- pages/01_ΏθΥΛ_Stats_Suite.py
+++
@@ -1,5 +0,0 @@
-from modules.common import init_page
-from modules.stats_suite import render
-
-init_page("Stats Suite")
-render()
--- pages/02_ΏθΤΛ_IVIVC_Suite.py
+++
@@ -1,5 +0,0 @@
-from modules.common import init_page
-from modules.ivivc_suite import render
-
-init_page("IVIVC Suite")
-render()
